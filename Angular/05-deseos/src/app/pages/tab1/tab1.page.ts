e7d3150e0e4127f875b55965b1613a50353dbb18
jayyei <sanchezrj0328@gmail.com>
2019-04-08 09:59:35 -0500

    Cambio

--- Angular/05-deseos/src/app/pages/tab1/tab1.page.ts
+++ Angular/05-deseos/src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { DeseosService } from 'src/app/services/deseos.service';
+import { Lista } from 'src/app/models/lista.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -7,8 +9,14 @@ import { DeseosService } from 'src/app/services/deseos.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  deseos: Lista[];
 
-  constructor(public deseosService: DeseosService) {
+  constructor(public deseosService: DeseosService,
+              private router: Router ) {
+      this.deseos = deseosService.listas;
+  }
 
+  agregarLista(){
+    this.router.navigateByUrl('/tabs/tab1/agregar');
   }
 }
