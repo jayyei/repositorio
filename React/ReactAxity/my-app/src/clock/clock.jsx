6c01ead207d302a72482c1a6a66f578ab7ceae73
jayyei <sanchezrj0328@gmail.com>
2019-04-30 19:57:41 -0500

    aAssbdjfhyu

--- React/ReactAxity/my-app/src/clock/clock.jsx
+++ React/ReactAxity/my-app/src/clock/clock.jsx
@@ -4,8 +4,10 @@ export class Clock extends Component{
     constructor(props){
         super(props);
         this.state = {
-            date: new Date()
-        }
+            date: new Date(),
+            isoFormat : false
+        };
+        //this.showMessage2() = this.showMessage2.bind(this);
     }
 
     componentDidMount(){
@@ -17,6 +19,7 @@ export class Clock extends Component{
 
     componentWillUpdate(){
         console.log("Actualizacion");
+        /* this.showMessage2(); */
     }
 
     componentWillUnmount(){
@@ -29,23 +32,46 @@ export class Clock extends Component{
         })
     }
 
+    showMessage(){
+        alert("Hola mundo");
+    }
+
+    showMessage2(){
+        alert("Hola mundo");
+    }
+
+    changeFormat(isoFormat2){
+        this.setState({
+            isoFormat : !isoFormat2
+        });
+    }
+    
+
     render(){
         const clockDiv = [
           <div>  
             <h1>Componente Reloj</h1>
-            <h1>Hora: { this.state.date.toLocaleTimeString() }</h1> 
+            <h1>Hora: { 
+                this.state.isoFormat ? 
+                this.state.date.toISOString() :
+                this.state.date.toLocaleTimeString() }</h1> 
          </div>
         ];
         
         const buttonDiv = [
             <div> 
-                <button>Presiona</button> 
+                <button onClick={this.changeFormat.bind(this, this.state.isoFormat)}>Presiona
+                { 
+                this.state.isoFormat ? 
+                ' Cambia a pesos' :
+                ' Cambiar a dolares' }
+                </button> 
            </div>
           ];
         return(
             <div>
                 {clockDiv}
-                {buttonDiv}    
+                {buttonDiv}  
             </div>
         )
     }
