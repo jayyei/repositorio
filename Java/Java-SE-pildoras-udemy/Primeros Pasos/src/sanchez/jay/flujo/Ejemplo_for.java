993950c0c54676d26e04d2fe2461da2b9960df2d
JAY_YEI <mrjay9628@gmail.com>
2019-09-01 23:30:37 -0500

    bucle for

---
+++ Java/Java-SE-pildoras-udemy/Primeros Pasos/src/sanchez/jay/flujo/Ejemplo_for.java
@@ -0,0 +1,13 @@
+package sanchez.jay.flujo;
+
+public class Ejemplo_for {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i= 0; i < 10; i++) {
+			System.out.println("Hola alumnos " + i);
+		}
+	}
+
+}
