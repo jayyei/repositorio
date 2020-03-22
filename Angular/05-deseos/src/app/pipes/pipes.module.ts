937459361e42b7642072e9af1e714e0d8c33a1c5
JAY_YEI <mrjay9628@gmail.com>
2020-03-21 20:19:44 -0600

    Se anade aplicacion de node y se borra angular

--- Angular/05-deseos/src/app/pipes/pipes.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FiltroCompletadoPipe } from './filtro-completado.pipe';
-
-@NgModule({
-  declarations: [FiltroCompletadoPipe],
-  exports: [FiltroCompletadoPipe]
-})
-export class PipesModule { }
