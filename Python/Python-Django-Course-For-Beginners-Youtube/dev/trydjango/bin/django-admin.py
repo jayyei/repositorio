38c6f05f79156d3ced75ce3e5f07f9becd268b91
Jay S <sanchezrj0328@gmail.com>
2021-01-28 04:43:36 -0600

    Correcion python

--- Python/Python-Django-Course-For-Beginners-Youtube/dev/trydjango/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/jay/Escritorio/Repositorio/Python/Python-Django-Course-For-Beginners-Youtube/dev/trydjango/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
