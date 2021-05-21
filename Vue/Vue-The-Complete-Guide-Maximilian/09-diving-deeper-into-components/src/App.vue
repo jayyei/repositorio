5df8521266e9b27cd7fbd9ab6a86805edfc91224
Jay S <sanchezrj0328@gmail.com>
2021-05-20 20:53:35 -0500

    Anadiendo todo

---
+++ Vue/Vue-The-Complete-Guide-Maximilian/09-diving-deeper-into-components/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <!-- or simply <TheHeader/> -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-else></manage-goals> -->
+    <!-- con component, te puedes deshacer de una manera elegante del v-if, v-else -->
+    <keep-alive>
+    <component :is="selectedComponent"></component>
+    </keep-alive>
+    <!-- Forma de usar scoped slots -->
+    <!-- <course-goals v-slot:default="slotProps">
+      <h2>{{ slotProps.item}}</h2>
+      <p> {{ slotProps.anotherProp}}</p>
+    </course-goals> -->
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/layout/TheHeader';
+import ActiveGoals from './components/ActiveGoals';
+import ManageGoals from './components/ManageGoals';
+
+export default {
+  components: {
+    'the-header': TheHeader,
+    // or simply TheHeader
+    ActiveGoals,
+    ManageGoals
+  },
+  data() {
+    return {
+      selectedComponent: 'active-goals',
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    }
+  }
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ Vue/Vue-The-Complete-Guide-Maximilian/09-diving-deeper-into-components/src/components/UserInfo.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <base-card>
+      <!-- #header is a shorthand of v-slot:header -->
+      <template #header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+      <template #default>
+        <p>{{ infoText }}</p>
+      </template>
+    </base-card>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['fullName', 'infoText', 'role'],
+};
+</script>
+
+<style scoped>
+/* if you use 'scoped', then this style will only affect the template it refers*/
+
+</style>
